5865077928e70bd10a549a456f232f9e0a616ee7
7db9a <repo.7db9a@gmail.com>
2022-05-31 00:11:43 -0400

    Create branches from script.

---
+++ demo-maker.sh
@@ -0,0 +1,18 @@
+# Clone a repo
+# git clone https... repo
+# Change remote origin to https://github.com/turbo-src/demo
+# create a pullRequests branch and create a new master many commits back.
+# Run `git cherry-pick <commit> to get commits you want` - 5 of them
+# Run this script.
+
+git -C repo checkout -b pullRequest5
+git -C repo checkout HEAD~1
+git -C repo checkout -b pullRequest4
+git -C repo checkout HEAD~1
+git -C repo checkout -b pullRequest3
+git -C repo checkout HEAD~1
+git -C repo checkout -b pullRequest2
+git -C repo checkout HEAD~1
+git -C repo checkout -b pullRequest1
+
+git
